474a77f1a8e39753b1617c7bf576266aa43767c9
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-05-25 02:45:16 +0300

    refactoring models

--- fast_api/src/models/person.py
+++ fast_api/src/models/person.py
@@ -1,31 +1,22 @@
 from typing import Optional
 from uuid import UUID
 
-import orjson
 from pydantic import BaseModel
+from json_config import BaseOrjsonModel
 
 
-def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
-    return orjson.dumps(v, default=default).decode()
-
-
-class Roles(BaseModel):
+class Roles(BaseModel, BaseOrjsonModel):
     role: str
     film_id: UUID
     film_title: Optional[str]
     film_imdb_rating: Optional[float]
 
 
-class RolesShort(BaseModel):
+class RolesShort(BaseModel, BaseOrjsonModel):
     role: str
     film_id: UUID
 
 
-class Person(BaseModel):
+class Person(BaseModel, BaseOrjsonModel):
     id: UUID
     full_name: str
-
-    class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
