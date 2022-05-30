0cb0661d22cab234eba5242976c590e8c5e37aa9
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-05-30 22:00:12 +0300

    api tests for genres all

--- fast_api/tests/functional/src/tests_main.py
+++ fast_api/tests/functional/src/tests_main.py
@@ -1,11 +1,25 @@
 import pytest
-from testdata.test_data_genres import genre_id_to_try
+from testdata.test_data_genres import genre_id_to_try, genres_all
 
-@pytest.mark.parametrize('genre_id, expected', genre_id_to_try)
-@pytest.mark.asyncio
-async def test_genre(make_get_request, genre_id, expected):
-    path = '/'.join(("/genres", genre_id))
-    response = await make_get_request(path)
 
-    assert response.status == 200
-    assert response.body == expected
+class GenreTests:
+
+    @pytest.mark.parametrize('genre_id, expected', genre_id_to_try)
+    @pytest.mark.asyncio
+    async def test_genre(self, make_get_request, genre_id, expected):
+        path = '/'.join(("/genres", genre_id))
+        response = await make_get_request(path)
+
+        assert response.status == 200
+        assert response.body == expected
+
+    @pytest.mark.parametrize('len_genres, expected', genres_all)
+    @pytest.mark.asyncio
+    async def test_genre_all(self, make_get_request, len_genres, expected):
+        path = "/genres"
+        response = await make_get_request(path)
+
+        assert response.status == 200
+        assert len(response.body) == len_genres
+        # check first 3 elements
+        assert response.body[0:3] == expected
