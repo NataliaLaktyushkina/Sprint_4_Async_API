1fec1a4d53935f6ed0f0e7ed7557fa23304efce9
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-05-31 22:50:28 +0300

    Api test for person id

---
+++ fast_api/tests/functional/testdata/test_data_person.py
@@ -0,0 +1,106 @@
+person_ids_for_test = [("5b4bf1bc-3397-4e83-9b17-8b10c6544ed1",
+                        {
+                            "uuid": "5b4bf1bc-3397-4e83-9b17-8b10c6544ed1",
+                            "full_name": "Harrison Ford",
+                            "films": [
+                                {
+                                    "role": "actor",
+                                    "film_id": "b6b8a3b7-1c12-45a8-9da7-4b20db8867df"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "3d825f60-9fff-4dfe-b294-1a45fa1e115d"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "cddf9b8f-27f9-4fe9-97cb-9e27d4fe3394"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "134989c3-3b20-4ae7-8092-3e8ad2333d59"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "3b1d0e70-42e5-4c9b-98cf-2681c420a99b"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "dbb9b244-483b-4592-9194-4938338419bc"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "f241a62c-2157-432a-bbeb-9c579c8bc18b"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "c7bd11a4-30bf-4077-a618-97c3e5525427"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "4f53452f-a402-4a76-89fd-f034eeb8d657"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "0312ed51-8833-413f-bff5-0e139c11264a"
+                                }
+                            ]
+                        }),
+                       ("b5d2b63a-ed1f-4e46-8320-cf52a32be358",
+                        {
+                            "uuid": "b5d2b63a-ed1f-4e46-8320-cf52a32be358",
+                            "full_name": "Carrie Fisher",
+                            "films": [
+                                {
+                                    "role": "actor",
+                                    "film_id": "3d825f60-9fff-4dfe-b294-1a45fa1e115d"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "cddf9b8f-27f9-4fe9-97cb-9e27d4fe3394"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "12a8279d-d851-4eb9-9d64-d690455277cc"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "46f15353-2add-415d-9782-fa9c5b8083d5"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "134989c3-3b20-4ae7-8092-3e8ad2333d59"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "f241a62c-2157-432a-bbeb-9c579c8bc18b"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "4f53452f-a402-4a76-89fd-f034eeb8d657"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "0312ed51-8833-413f-bff5-0e139c11264a"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "025c58cd-1b7e-43be-9ffb-8571a613579b"
+                                }
+                            ]
+                        }),
+                       ("e039eedf-4daf-452a-bf92-a0085c68e156",
+                        {
+                            "uuid": "e039eedf-4daf-452a-bf92-a0085c68e156",
+                            "full_name": "Peter Cushing",
+                            "films": [
+                                {
+                                    "role": "actor",
+                                    "film_id": "3d825f60-9fff-4dfe-b294-1a45fa1e115d"
+                                },
+                                {
+                                    "role": "actor",
+                                    "film_id": "73ecd1e6-6326-405a-b51b-69008f383b72"
+                                }
+                            ]
+                        }
+                        )]
